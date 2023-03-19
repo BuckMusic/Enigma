2366ff37cc5b35e24ddc7afda1ec05e49f82c5c2
MMuttel <BuckyFeedback@gmail.com>
2023-03-19 13:05:01 +0000

    Add files via upload

---
+++ src/components/Dropdown22.js
@@ -0,0 +1,18 @@
+
+
+import Dropdown2 from './Dropdown2'
+
+function Dropdown22() {
+    return (
+      <div className='Dropdown22'>
+        
+            <Dropdown2 />
+            <Dropdown2 />
+            <Dropdown2 />
+        
+
+      </div>
+    )
+}
+
+export default Dropdown22
